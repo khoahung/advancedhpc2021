a8bdcae67763cd3bbdab067e1b09f559ccf39a18
Nguyễn Tất Hùng <hung.nguyen@bluecoglobal.com>
2021-10-31 17:40:11 +0700

    update report

--- labwork/src/labwork.cu
+++ labwork/src/labwork.cu
@@ -152,7 +152,12 @@ void Labwork::labwork2_GPU() {
     }
 
 }
-
+__global__ void grayscale(uchar3 *input, uchar3 *output) {
+       int tid = threadIdx.x + blockIdx.x * blockDim.x;
+       output[tid].x = (input[tid].x + input[tid].y +
+                       input[tid].z) / 3;
+       output[tid].z = output[tid].y = output[tid].x;
+}
 void Labwork::labwork3_GPU() {
     // Calculate number of pixels
 
